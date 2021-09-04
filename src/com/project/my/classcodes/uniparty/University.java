a0d5ecf27a92d727754fe820d3c6da35738d5511
mmirzay <mirzay.mohsen@gmail.com>
2021-09-05 00:00:11 +0430

    added entity for University

---
+++ src/com/project/my/classcodes/uniparty/University.java
@@ -0,0 +1,10 @@
+package com.project.my.classcodes.uniparty;
+
+public class University {
+	private Employee[] employees;
+	private int addedEmployees;
+
+	public University(int numberOfEmployees) {
+		this.employees = new Employee[numberOfEmployees];
+	}
+}
