affced05aa4b040f99883e68966f1f7b8c172161
mmirzay <mirzay.mohsen@gmail.com>
2021-09-05 00:00:37 +0430

    added Location propeties.

--- src/com/project/my/classcodes/uniparty/Location.java
+++ src/com/project/my/classcodes/uniparty/Location.java
@@ -1,5 +1,25 @@
 package com.project.my.classcodes.uniparty;
 
 public class Location {
+	private static final double UNIVERSIT_LATITUDE = 35.7036;
+	private static final double UNIVERSIT_LONGITUDE = 51.3515;
 
+	private double latitude;
+	private double longitude;
+
+	public double getLatitude() {
+		return latitude;
+	}
+
+	public void setLatitude(double latitude) {
+		this.latitude = latitude;
+	}
+
+	public double getLongitude() {
+		return longitude;
+	}
+
+	public void setLongitude(double longitude) {
+		this.longitude = longitude;
+	}
 }
