8317f7b2df7310306291e76315d0ace49127aafa
mmirzay <mirzay.mohsen@gmail.com>
2021-09-05 15:01:22 +0430

    added showInvitationList method to University.java

--- src/com/project/my/classcodes/uniparty/University.java
+++ src/com/project/my/classcodes/uniparty/University.java
@@ -8,13 +8,18 @@ public class University {
 		this.employees = new Employee[numberOfEmployees];
 	}
 
-	public void addEmployee(Employee employee) {
+	public void addEmployee(Employee newEmployee) {
 		if (addedEmployees >= employees.length) {
 			System.out.println("All employees are added.");
 			return;
 		}
-		employees[addedEmployees++] = employee;
-		System.out.println(employee + " added.");
+		for (Employee employee : employees)
+			if (employee != null && employee.getId() == newEmployee.getId()) {
+				System.out.println("Id is duplicat!");
+				return;
+			}
+		employees[addedEmployees++] = newEmployee;
+		System.out.println(newEmployee + " added.");
 	}
 
 	public void addSpouseById(int id, Name spouseName) {
@@ -24,7 +29,7 @@ public class University {
 				System.out.println(spouseName + "added as spouse of " + employee.getName());
 				return;
 			}
-		System.out.println("No such Id exists.");
+		System.out.println("No such Id exists!");
 	}
 
 	public void showMarriageStatusById(int id) {
@@ -33,16 +38,25 @@ public class University {
 				System.out.println("This employee is " + employee.getMarriageStatus());
 				return;
 			}
-		System.out.println("No such Id exists.");
+		System.out.println("No such Id exists!");
 	}
 
 	public void showInvitationStatusById(int id) {
 		for (Employee employee : employees)
 			if (employee != null && employee.getId() == id) {
-				System.out.printf("This employee is %s to the party.",
+				System.out.printf("This employee is %s to the party.%n",
 						Party.isInvited(employee) ? "invited" : "NOT invited");
 				return;
 			}
-		System.out.println("No such Id exists.");
+		System.out.println("No such Id exists!");
+	}
+
+	public void showInvitationList() {
+		int counter = 0;
+		System.out.println("Inivtation List:");
+		for (Employee employee : employees)
+			if (employee != null && Party.isInvited(employee))
+				System.out.printf("#%d- %s%n", ++counter, employee);
+		System.out.println("No one is invited!");
 	}
 }
