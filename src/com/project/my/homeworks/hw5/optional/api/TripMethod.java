9dcfe20592cc81f3d32cee5d137637c8c2484f8c
mmirzay <mirzay.mohsen@gmail.com>
2021-09-07 17:17:12 +0430

    added TripMethod enum.

---
+++ src/com/project/my/homeworks/hw5/optional/api/TripMethod.java
@@ -0,0 +1,5 @@
+package com.project.my.homeworks.hw5.optional.api;
+
+public enum TripMethod {
+	Economic, Vip, Bike;
+}
--- src/com/project/my/homeworks/hw5/optional/entities/OnlineTaxi.java
+++ src/com/project/my/homeworks/hw5/optional/entities/OnlineTaxi.java
@@ -1,8 +1,10 @@
 package com.project.my.homeworks.hw5.optional.entities;
 
+import com.project.my.homeworks.hw5.optional.api.TripMethod;
+
 public class OnlineTaxi {
 
-	public int getPrice(String type, TripParameters tripParameters) {
+	public int getPrice(TripMethod tripMethod, TripParameters tripParameters) {
 		return 0;
 	}
 }
