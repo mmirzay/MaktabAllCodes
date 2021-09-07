dd71f25208eb42600c7cab34c5b370632ce362e0
mmirzay <mirzay.mohsen@gmail.com>
2021-09-07 22:05:49 +0430

    revised isPrime method to be more performant.

--- src/com/project/my/homeworks/hw5/q1/Main.java
+++ src/com/project/my/homeworks/hw5/q1/Main.java
@@ -23,16 +23,16 @@ public class Main {
 	private static boolean isPrime(int num, int... i) {
 		if (num == 2)
 			return true;
-		if (num < 2)
+		if (num < 2 || num % 2 == 0)
 			return false;
 		if (i.length == 0) {
 			i = new int[1];
-			i[0] = (int) Math.sqrt(num);
+			i[0] = 2;
 		}
-		if (i[0] < 2)
+		if (i[0] * i[0] > num)
 			return true;
 		if (num % i[0] == 0)
 			return false;
-		return isPrime(num, i[0] - 1);
+		return isPrime(num, i[0] + 1);
 	}
 }
