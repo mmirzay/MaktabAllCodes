83ed9ef5b53a52fa4b973de42064ad0582ae0383
mmirzay <mirzay.mohsen@gmail.com>
2021-09-16 19:40:53 +0430

    added Main class to hw6.

--- src/com/project/my/homeworks/hw6/q1/Main.java
+++ src/com/project/my/homeworks/hw6/q1/Main.java
@@ -1,5 +1,11 @@
 package com.project.my.homeworks.hw6.q1;
 
+import com.project.my.homeworks.hw6.q1.frontend.ui.UIManager;
+
 public class Main {
 
+	public static void main(String[] args) {
+		UIManager onlineMarketUi = new UIManager();
+		onlineMarketUi.run();
+	}
 }
