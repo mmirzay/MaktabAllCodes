097bd405a4ef7eb2900ee412a6e13405d89673ad
mmirzay <mirzay.mohsen@gmail.com>
2021-09-17 20:12:41 +0430

    added addNewItem method to UIManger in hw6.

--- src/com/project/my/homeworks/hw6/q1/frontend/ui/UIManager.java
+++ src/com/project/my/homeworks/hw6/q1/frontend/ui/UIManager.java
@@ -3,6 +3,20 @@ package com.project.my.homeworks.hw6.q1.frontend.ui;
 import java.util.Scanner;
 
 import com.project.my.homeworks.hw6.q1.backend.services.OnlineMarket;
+import com.project.my.homeworks.hw6.q1.backend.services.entities.item.AnalogRadio;
+import com.project.my.homeworks.hw6.q1.backend.services.entities.item.Book;
+import com.project.my.homeworks.hw6.q1.backend.services.entities.item.DigitalRadio;
+import com.project.my.homeworks.hw6.q1.backend.services.entities.item.FormalShoe;
+import com.project.my.homeworks.hw6.q1.backend.services.entities.item.FormalShoeType;
+import com.project.my.homeworks.hw6.q1.backend.services.entities.item.Item;
+import com.project.my.homeworks.hw6.q1.backend.services.entities.item.ItemCategory;
+import com.project.my.homeworks.hw6.q1.backend.services.entities.item.LcdTV;
+import com.project.my.homeworks.hw6.q1.backend.services.entities.item.LedTV;
+import com.project.my.homeworks.hw6.q1.backend.services.entities.item.Magazine;
+import com.project.my.homeworks.hw6.q1.backend.services.entities.item.MagazineType;
+import com.project.my.homeworks.hw6.q1.backend.services.entities.item.Newspaper;
+import com.project.my.homeworks.hw6.q1.backend.services.entities.item.SportShoe;
+import com.project.my.homeworks.hw6.q1.backend.services.entities.user.Customer;
 
 public class UIManager {
 	private Scanner input;
@@ -23,15 +37,15 @@ public class UIManager {
 
 	}
 
-	private String getOptionalString(String msg) {
+	private String getOptionalStringInputValue(String msg) {
 		System.out.print(msg + " ");
 		return input.nextLine();
 	}
 
-	private String getString(String msg) {
+	private String getStringInputValue(String msg) {
 		String result = null;
 		while (true) {
-			result = getOptionalString(msg);
+			result = getOptionalStringInputValue(msg);
 			if (result.isEmpty())
 				printErrorMessage("Invalid input!");
 			else
@@ -39,10 +53,10 @@ public class UIManager {
 		}
 	}
 
-	private int getIntValue(String msg) {
+	private int getIntInputValue(String msg) {
 		while (true)
 			try {
-				return Integer.parseInt(getString(msg));
+				return Integer.parseInt(getStringInputValue(msg));
 			} catch (Exception e) {
 				printErrorMessage("Invalid input!");
 				if (msg.isEmpty())
@@ -55,12 +69,12 @@ public class UIManager {
 		printTitle("Login Page");
 		System.out.println("1- Login as Admin");
 		System.out.println("2- Login as Customer");
-		System.out.println("3- Sign up a new Customer");
+		System.out.println("3- Sign up");
 		System.out.println("4- Exit");
 		printDashedBorder();
 		System.out.print(">> ");
 
-		switch (getIntValue("")) {
+		switch (getIntInputValue("")) {
 		case 1:
 			loginAsAdmin();
 			break;
@@ -80,8 +94,8 @@ public class UIManager {
 
 	private void loginAsAdmin() {
 		printTitle("Login as Admin");
-		String username = getString("Enter username:");
-		String password = getString("Enter password:");
+		String username = getStringInputValue("Enter username:");
+		String password = getStringInputValue("Enter password:");
 		if (onlineMarket.getUsersManager().loginAdmin(username, password))
 			showAdminPage();
 		else
@@ -90,8 +104,8 @@ public class UIManager {
 
 	private void loginAsCustomer() {
 		printTitle("Login as Customer");
-		String username = getString("Enter username:");
-		String password = getString("Enter username:");
+		String username = getStringInputValue("Enter username:");
+		String password = getStringInputValue("Enter username:");
 		if (onlineMarket.getUsersManager().loginUser(username, password))
 			showUserPage();
 		else
@@ -100,14 +114,22 @@ public class UIManager {
 
 	private void signUp() {
 		printTitle("Sign Up");
-		String username = getString("Enter Username:");
-		String password = getString("Enter Password:");
-		String firstName = getString("Enter First Name:");
-		String lastName = getString("Enter Last Name:");
-		if (onlineMarket.getUsersManager().addNewCustomer(username, password, firstName, lastName))
-			printInfoMessage("Signed up successfully.");
-		else
+		if (onlineMarket.getUsersManager().isCutomerArrayFull()) {
+			printErrorMessage("Customers array is full.");
+			return;
+		}
+		String username = getStringInputValue("Enter Username:");
+		if (onlineMarket.getUsersManager().isDuplicateUsername(username)) {
 			printErrorMessage("Username is duplicate. Try again please.");
+			return;
+		}
+		String password = getStringInputValue("Enter Password:");
+		String firstName = getStringInputValue("Enter First Name:");
+		String lastName = getStringInputValue("Enter Last Name:");
+		Customer customer = new Customer(username, password, firstName, lastName);
+		onlineMarket.getUsersManager().addNewCustomer(customer);
+		printInfoMessage("Signed up successfully.");
+
 	}
 
 	private void stop() {
@@ -122,11 +144,186 @@ public class UIManager {
 		System.out.println("4- Back");
 		printDashedBorder();
 		System.out.print(">> ");
-		switch (getIntValue("")) {
+		switch (getIntInputValue("")) {
+		case 1:
+			addNewItem();
+			break;
 		case 4:
 			return;
+		default:
+			printErrorMessage("Invalid selection. Try again please!");
 		}
 		showAdminPage();
+	}
+
+	private void addNewItem() {
+		printTitle("Add New Item");
+		if (onlineMarket.getItemManager().isItemArrayFull()) {
+			printErrorMessage("Items array is full");
+			return;
+		}
+		ItemCategory selectedItem = selectItemCategory();
+		String code = getStringInputValue("Enter item code:");
+		if (onlineMarket.getItemManager().isDuplicateCode(code)) {
+			printErrorMessage(code + " is duplicate code. Try again.");
+			return;
+		}
+
+		int price = getIntInputValue("Enter item price:");
+		Item item = new Item(code, price);
+
+		switch (selectedItem) {
+		case BOOK:
+			item = getBook(item);
+			break;
+		case NEWSPAPER:
+			item = getNewspaper(item);
+			break;
+		case MAGAZINE:
+			item = getMagazine(item);
+			break;
+		case FORMAL_SHOE:
+			item = getFormalShoe(item);
+			break;
+		case SPORT_SHOE:
+			item = getSportShoe(item);
+			break;
+		case ANALOG_RADIO:
+			item = getAnalogRadio(item);
+			break;
+		case DIGITAL_RADIO:
+			item = getDigitalRadio(item);
+			break;
+		case LCD_TV:
+			item = getLcdTV(item);
+			break;
+		case LED_TV:
+			item = getLedTV(item);
+			break;
+		}
+
+		onlineMarket.getItemManager().addNewItem(item);
+		printInfoMessage("Item added succesffuly");
+
+	}
+
+	private ItemCategory selectItemCategory() {
+		System.out.println("Select an Item type: ");
+		System.out.println(" + Readables: ");
+		System.out.println("     1- Book");
+		System.out.println("     2- Newspaper");
+		System.out.println("     3- Magazine");
+		System.out.println(" + Shoes: ");
+		System.out.println("     4- Formal");
+		System.out.println("     5- Sport");
+		System.out.println(" + Electrical: ");
+		System.out.println("     6- Analog Radio");
+		System.out.println("     7- Digital Radio");
+		System.out.println("     8- LCD TV");
+		System.out.println("     9- LED TV");
+		switch (getIntInputValue(">> ")) {
+		case 1:
+			return ItemCategory.BOOK;
+		case 2:
+			return ItemCategory.NEWSPAPER;
+		case 3:
+			return ItemCategory.MAGAZINE;
+		case 4:
+			return ItemCategory.FORMAL_SHOE;
+		case 5:
+			return ItemCategory.SPORT_SHOE;
+		case 6:
+			return ItemCategory.ANALOG_RADIO;
+		case 7:
+			return ItemCategory.DIGITAL_RADIO;
+		case 8:
+			return ItemCategory.LCD_TV;
+		case 9:
+			return ItemCategory.LED_TV;
+		default:
+			printErrorMessage("Invalid selection. Try again please!");
+		}
+		return selectItemCategory();
+	}
+
+	private Item getBook(Item item) {
+		String title = getStringInputValue("Enter book title:");
+		String authorName = getStringInputValue("Enter book author name:");
+		Book book = new Book(title, authorName, item.getCode(), item.getPrice());
+		return book;
+	}
+
+	private Item getNewspaper(Item item) {
+		String title = getStringInputValue("Enter newspaper title:");
+		String date = getStringInputValue("Enter newspaper date (Ex. 09-17-2021):");
+		Newspaper newspaper = new Newspaper(title, date, item.getCode(), item.getPrice());
+		return newspaper;
+	}
+
+	private Item getMagazine(Item item) {
+		String title = getStringInputValue("Enter newspaper title:");
+		String strMagazineType = getOptionalStringInputValue("Enter magazine type (General, Cooking, Fashion, Tech):")
+				.toLowerCase();
+		MagazineType magazineType = MagazineType.GENERAL;
+		if (strMagazineType.startsWith("c"))
+			magazineType = MagazineType.COOKING;
+		else if (strMagazineType.startsWith("f"))
+			magazineType = MagazineType.FASHION;
+		else if (strMagazineType.startsWith("t"))
+			magazineType = MagazineType.TECHNOLOGY;
+
+		Magazine magazine = new Magazine(title, magazineType, item.getCode(), item.getPrice());
+		return magazine;
+	}
+
+	private Item getFormalShoe(Item item) {
+		int size = getIntInputValue("Enter fromal shoe size:");
+		String color = getStringInputValue("Enter formal shoe color:");
+		String strFormalShoeType = getOptionalStringInputValue("Enter formal shoe type (Kids, Women, Men):")
+				.toLowerCase();
+		FormalShoeType formalShoeType = FormalShoeType.MEN;
+		if (strFormalShoeType.startsWith("w"))
+			formalShoeType = FormalShoeType.WOMEN;
+		else if (strFormalShoeType.startsWith("k"))
+			formalShoeType = FormalShoeType.KIDS;
+		FormalShoe formalShoe = new FormalShoe(color, formalShoeType, size, item.getCode(), item.getPrice());
+		return formalShoe;
+	}
+
+	private Item getSportShoe(Item item) {
+		int size = getIntInputValue("Enter sport shoe size:");
+		String color = getStringInputValue("Enter sport shoe color:");
+		SportShoe sportShoe = new SportShoe(color, size, item.getCode(), item.getPrice());
+		return sportShoe;
+	}
+
+	private Item getAnalogRadio(Item item) {
+		String mark = getStringInputValue("Enter analog radio mark:");
+		AnalogRadio analogRadio = new AnalogRadio(mark, item.getCode(), item.getPrice());
+		return analogRadio;
+	}
+
+	private Item getDigitalRadio(Item item) {
+		String mark = getStringInputValue("Enter digital radio mark:");
+		boolean isChargeble = getOptionalStringInputValue("Is chargeable digital radio (yes/no):").toLowerCase()
+				.startsWith("y");
+		DigitalRadio digitalRadio = new DigitalRadio(mark, isChargeble, item.getCode(), item.getPrice());
+		return digitalRadio;
+	}
+
+	private Item getLcdTV(Item item) {
+		String mark = getStringInputValue("Enter LCD TV mark:");
+		int size = getIntInputValue("Enter LCD TV size inch:");
+		LcdTV lcdTV = new LcdTV(mark, size, item.getCode(), item.getPrice());
+		return lcdTV;
+	}
+
+	private Item getLedTV(Item item) {
+		String mark = getStringInputValue("Enter LED TV mark:");
+		int size = getIntInputValue("Enter LED TV size inch:");
+		boolean isSmart = getOptionalStringInputValue("is Smart LED TV (yes/no):").toLowerCase().startsWith("y");
+		LedTV ledTV = new LedTV(mark, isSmart, size, item.getCode(), item.getPrice());
+		return ledTV;
 	}
 
 	private void showUserPage() {
