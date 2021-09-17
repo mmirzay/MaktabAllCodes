45a18ad5236c39ad70dc6cface53d510cc57a566
mmirzay <mirzay.mohsen@gmail.com>
2021-09-17 20:11:04 +0430

    revised Magazine types in hw6.

--- src/com/project/my/homeworks/hw6/q1/backend/services/entities/item/MagazineType.java
+++ src/com/project/my/homeworks/hw6/q1/backend/services/entities/item/MagazineType.java
@@ -1,5 +1,5 @@
 package com.project.my.homeworks.hw6.q1.backend.services.entities.item;
 
 public enum MagazineType {
-	GENERAL, COOKING, ART, SPORT, FASHION, TECHNOLOGY;
+	GENERAL, COOKING, FASHION, TECHNOLOGY;
 }
