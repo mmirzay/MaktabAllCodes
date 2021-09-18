9c75b361287797b9139d8a03b3ded883bb2d871e
mmirzay <mirzay.mohsen@gmail.com>
2021-09-18 23:28:07 +0430

    chaned max number of users in OnlineMarket in hw6.

--- src/com/project/my/homeworks/hw6/q1/backend/services/Constants.java
+++ src/com/project/my/homeworks/hw6/q1/backend/services/Constants.java
@@ -1,7 +1,7 @@
 package com.project.my.homeworks.hw6.q1.backend.services;
 
 public class Constants {
-	public static final int MAX_NUMBER_OF_USERS = 10;
+	public static final int MAX_NUMBER_OF_USERS = 5;
 	public static final int MAX_NUMBER_OF_ITEMS = 100;
 	public static final int SHOPPING_BASKET_MAX_SIZE = 5;
 	private static final String SPACES = "%-15s";
