f3b2d7c3a46e37e5e9089ea54af6bcbedb277f95
mmirzay <mirzay.mohsen@gmail.com>
2021-09-18 22:11:59 +0430

    added max number of users and items to Constants in hw6.

--- src/com/project/my/homeworks/hw6/q1/backend/services/OnlineMarket.java
+++ src/com/project/my/homeworks/hw6/q1/backend/services/OnlineMarket.java
@@ -4,14 +4,14 @@ import com.project.my.homeworks.hw6.q1.backend.managers.ItemsManager;
 import com.project.my.homeworks.hw6.q1.backend.managers.UsersManager;
 
 public class OnlineMarket {
-	private static final int NUMBER_OF_ALL_CUSTOMERS = 10;
-	private static final int NUMBER_OF_ALL_ITEMS = 100;
+	private int maxNumberOfUsers = Constants.MAX_NUMBER_OF_USERS;
+	private int maxNumberOfItems = Constants.MAX_NUMBER_OF_ITEMS;
 	private UsersManager userManager;
 	private ItemsManager itemsManager;
 
 	public OnlineMarket() {
-		userManager = new UsersManager(NUMBER_OF_ALL_CUSTOMERS);
-		itemsManager = new ItemsManager(NUMBER_OF_ALL_ITEMS);
+		userManager = new UsersManager(maxNumberOfUsers);
+		itemsManager = new ItemsManager(maxNumberOfItems);
 	}
 
 	public UsersManager getUsersManager() {
