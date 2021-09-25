202a93337600809742efa64e043864e6010eb31e
mmirzay <mirzay.mohsen@gmail.com>
2021-09-25 17:47:40 +0330

    added Database connection classes to hw7.

---
+++ src/com/project/my/homeworks/hw7/q1/DbConfig.java
@@ -0,0 +1,7 @@
+package com.project.my.homeworks.hw7.q1;
+
+public class DbConfig {
+	static final String URL = "jdbc:mysql://...";
+	public static final String USERNAME = "...";
+	public static final String PASSWORD = "...";
+}
---
+++ src/com/project/my/homeworks/hw7/q1/DbConnection.java
@@ -0,0 +1,22 @@
+package com.project.my.homeworks.hw7.q1;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+
+import javax.sql.DataSource;
+
+import com.mysql.cj.jdbc.MysqlDataSource;
+
+public class DbConnection {
+	private static DataSource dataSource;
+
+	public static Connection getConnection() throws SQLException {
+		if (dataSource == null) {
+			dataSource = new MysqlDataSource();
+			((MysqlDataSource) dataSource).setUrl(DbConfig.URL);
+			((MysqlDataSource) dataSource).setUser(DbConfig.USERNAME);
+			((MysqlDataSource) dataSource).setPassword(DbConfig.PASSWORD);
+		}
+		return dataSource.getConnection();
+	}
+}
