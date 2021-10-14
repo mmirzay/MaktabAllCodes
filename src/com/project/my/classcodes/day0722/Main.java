3b08333df38cb39624148606175337aca1ddc18d
mmirzay <mirzay.mohsen@gmail.com>
2021-10-14 11:24:54 +0330

    checked threads priorities simply.

--- src/com/project/my/classcodes/day0722/Main.java
+++ src/com/project/my/classcodes/day0722/Main.java
@@ -2,9 +2,12 @@ package com.project.my.classcodes.day0722;
 
 public class Main {
 	public static void main(String[] args) {
-		System.out.println("main");
-		Thread.currentThread().setPriority(1);
-		MyThread myThread = new MyThread(new MyRunnable());
+		MyThread myThread = new MyThread();
+		myThread.setPriority(Thread.MAX_PRIORITY);
 		myThread.start();
+		System.out.println("hello");
+		System.out.println(Thread.currentThread().getName());
+		Thread.currentThread().setPriority(Thread.MIN_PRIORITY);
+		System.out.println("done");
 	}
 }
--- src/com/project/my/classcodes/day0722/MyThread.java
+++ src/com/project/my/classcodes/day0722/MyThread.java
@@ -1,12 +1,13 @@
 package com.project.my.classcodes.day0722;
 
-public class MyThread extends Thread{
-	public MyThread(MyRunnable myRunnable) {
-		super(myRunnable);
+public class MyThread extends Thread {
+
+	public MyThread() {
+		super();
 	}
 
 	@Override
 	public void run() {
-		System.out.println("bye: "+Thread.currentThread().getName());
+		System.out.println("MyThread: " + this.getName());
 	}
 }
